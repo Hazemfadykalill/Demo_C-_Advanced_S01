6556c1e67cacaf86f45269ec059545457528ad0d
Hazem Fady <hazemfady123@gmail.com>
2024-07-28 17:40:01 +0300

    Add project files.

---
+++ Demo_C#_Advanced_S01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_C__Advanced_S01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
